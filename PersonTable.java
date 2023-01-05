d804a21af14d50cde20805b286ac4a2186cfd367
Frankie Chow <freakchow@gmail.com>
2023-01-05 17:24:32 +0000

    Add the Person Table and create the msg textarea.

---
+++ PersonTable.java
@@ -0,0 +1,50 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.table.*;
+import java.util.Vector ;
+
+public class PersonTable extends JTable {
+    DefaultTableModel personModel;
+    TabFrame mainFrame;
+    MysqlHandler dbHandler;
+    Vector<String> tableHeader ;
+    
+    PersonTable(TabFrame motherFrame, MysqlHandler dbPosie) {
+        super();
+        this.dbHandler = dbPosie ;
+        createTableHeader();
+        createTable();
+    }
+    
+    void createTableHeader() {
+        Vector<String> headerTitle = new Vector<String>();
+        headerTitle.add("id");
+        headerTitle.add("FirstName");
+        headerTitle.add("SurName");
+        headerTitle.add("Telephone");
+        headerTitle.add("EmailAddress");
+        headerTitle.add("PhysicalAddress");
+        this.tableHeader = headerTitle ;
+    }
+    
+    void flashTable () {
+        reNewTable ( this.dbHandler.selectPersonRecord() );
+    }
+    
+    void reNewTable( Vector<Vector<String>>  dbRow ) {
+        this.personModel.setDataVector( dbRow , this.tableHeader );
+    }
+    
+    void createTable() {
+        this.tableHeader = new Vector<String>();
+        this.tableHeader.add("id");
+        this.tableHeader.add("FirstName");
+        this.tableHeader.add("SurName");
+        this.tableHeader.add("Telephone");
+        this.tableHeader.add("EmailAddress");
+        this.tableHeader.add("PhysicalAddress");
+
+        this.personModel = new DefaultTableModel( this.dbHandler.selectPersonRecord() ,this.tableHeader);
+        super.setModel(this.personModel);
+    }
+}
